e42efc4fcea0dc39528bc6eaca8410a70e9621cb
frinzekt <frinzekt@gmail.com>
2020-04-09 00:41:32 +0800

    for loop table generation using object passed

--- Exercises/enrollment/app/routes.py
+++ Exercises/enrollment/app/routes.py
@@ -9,13 +9,15 @@ def index():
 
 @app.route("/login")
 def login():
-    return render_template("login.html", login=True)
+    return render_template("login.html" )
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html", login=True)
+    courseData= [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
+    print(courseData)
+    return render_template("courses.html",courseData=courseData )
 
 @app.route("/register")
 def register():
-    return render_template("register.html", login=True)
+    return render_template("register.html")
 
