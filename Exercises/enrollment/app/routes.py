f8be65817831d1024aa83f095df7a6d4c3fa7bcc
frinzekt <frinzekt@gmail.com>
2020-04-08 23:49:52 +0800

    templates and static files

--- Exercises/enrollment/app/routes.py
+++ Exercises/enrollment/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return"<h1>Hello Eart1h!</h1>"
+    return render_template("index.html")
