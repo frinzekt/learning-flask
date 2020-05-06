9e23ec5a8d4ff10065e2ab680c3c0efab0987067
frinzekt <frinzekt@gmail.com>
2020-05-07 01:10:41 +0800

    updating to accomodate sessions per user

--- Exercises/flask socket io/app/routes.py
+++ Exercises/flask socket io/app/routes.py
@@ -12,17 +12,23 @@ def index():
     return render_template("index.html", index=True, async_mode=socketio.async_mode)
 
 #SOCKETS
+@socketio.on('connectFirstTime', namespace='/test')
+def connectFirstTime(message):
+    session[str(message["userId"])] = 0;
+    emit('my_response',{'data': message['data'] + " By: " + str(message["userId"]) + " *score = " + str(session[str(message["userId"])])})
 
 @socketio.on('my_event', namespace='/test')
 def test_message(message):
+    session[str(message["userId"])] += 5;
+    print(session[str(message["userId"])])
     emit('my_response',
-         {'data': message['data']})
+         {'data': message['data'] + " By: " + str(message["userId"]) + " *score = " + str(session[str(message["userId"])])})
 
 
 @socketio.on('my_broadcast_event', namespace='/test')
 def test_broadcast_message(message):
     emit('my_response',
-         {'data': message['data']},
+         {'data': message['data'] + " By: " + str(message["userId"]) + " *score = " + str(session[str(message["userId"])])},
          broadcast=True)
 
 
@@ -37,12 +43,12 @@ def join(message):
 def leave(message):
     leave_room(message['room'])
     emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms())})
+         {'data': 'Left rooms: ' + ', '.join(rooms())})
 
 
 @socketio.on('my_room_event', namespace='/test')
 def send_room_message(message):
-    emit('my_response',{'data': message['data']},room=message['room'])
+    emit('my_response',{'data': message['data'] + " By: " + str(message["userId"]) + " *score = " + str(session[str(message["userId"])])},room=message['room'])
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
